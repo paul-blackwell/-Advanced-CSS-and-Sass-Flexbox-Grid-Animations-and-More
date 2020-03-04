a7b6ee0b6dc5ba0712cd56c1fcb3444bd5db461f
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-03-04 07:03:23 +0000

    Converting Our CSS Code to Sass: Variables and Nesting

---
+++ Natours/starter/test.js
@@ -0,0 +1,14 @@
+{
+{
+    type: 'doughnut',
+    data: {
+        labels: ['Okay', 'Expired', 'Failed', 'Missing', 'Other'],
+        datasets: [{
+            label: 'Current',
+            data: [0, 19, 0, 19, 0],
+            backgroundColor: ['rgba(63, 197, 90, 0.6)', 'rgba(255, 193, 4, 0.5)', 'rgba(253, 63, 68, .5)', 'rgba(220, 225, 228, .5)', 'rgba(29, 172, 252, 0.5)'],
+          fill: '-1'
+        }]
+    }
+} 
+}
